3a6b4ce33b97e4aacfed99d28d7b1cc5c43138b4
songhuixiang <songhuixiang@hulai.com>
2021-07-30 19:29:03 +0800

    add FallbackNode

---
+++ src/controls/FallbackNode.ts
@@ -0,0 +1,50 @@
+import { NodeStatus } from '../BasicTypes';
+import { ControlNode } from '../ControlNode';
+
+export abstract class FallbackNode extends ControlNode<{}> {
+    private current_child_idx_: number;
+
+    constructor(name: string) {
+        super(name, {});
+        this.current_child_idx_ = 0;
+        this.setRegistrationID('Fallback');
+    }
+
+    protected tick(): NodeStatus {
+        const children_count = this.children_nodes_.length;
+
+        this.setStatus(NodeStatus.RUNNING);
+
+        while (this.current_child_idx_ < children_count) {
+            const current_child_node = this.children_nodes_[this.current_child_idx_];
+            const child_status = current_child_node.executeTick();
+
+            switch (child_status) {
+                case NodeStatus.RUNNING:
+                    return child_status;
+                case NodeStatus.SUCCESS:
+                    this.haltChildren();
+                    this.current_child_idx_ = 0;
+                    return child_status;
+                case NodeStatus.FAILURE:
+                    this.current_child_idx_++;
+                    break;
+                case NodeStatus.IDLE:
+                    throw new Error('A child node must never return IDLE');
+            }
+        }
+
+        // The entire while loop completed. This means that all the children returned FAILURE.
+        if (this.current_child_idx_ == children_count) {
+            this.haltChildren();
+            this.current_child_idx_ = 0;
+        }
+
+        return NodeStatus.FAILURE;
+    }
+
+    public halt() {
+        this.current_child_idx_ = 0;
+        super.halt();
+    }
+}
